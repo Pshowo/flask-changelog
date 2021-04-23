36a79e41bf35613bae3dd3832af65fdad4e55b1f
Pawel <pjuszcze@gmail.com>
2021-04-23 19:42:18 +0200

    Changed: format html

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import flask
+from flask import render_template, redirect, url_for
+
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Flask changelog"
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
