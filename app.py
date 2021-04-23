7124aa7dc0676809543db6d1dc57174d9332a739
Pawel <pjuszcze@gmail.com>
2021-04-23 22:19:18 +0200

    Added: POST form

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    print(request.query_string)
+
     prog_dict = {
         'program': 0,
         'name': 'Program 1',
@@ -15,15 +15,18 @@ def index():
         'tab1': '',
     }
     if 'pr' in request.args and int(request.args['pr']) == 1:
-        ic()
         prog_dict['program'] = int(request.args['pr'])
         prog_dict['name'] = "Program 2"
         prog_dict['tab0'] = ''
         prog_dict['tab1'] = 'active'
-    print(prog_dict)
 
     return render_template('base.html', pr=prog_dict)
 
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    for p in request.form:
+        print(p, request.form[p])
 
+    return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run()
