1cc8cf97b9fd1ba577b5948b195c8fd794c32858
Pawel <pjuszcze@gmail.com>
2021-04-23 21:02:52 +0200

    Added: switched between tabs

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import flask
-from flask import render_template, redirect, url_for
-
+from flask import render_template, redirect, url_for, request
+from icecream import ic
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('base.html')
+    print(request.query_string)
+    prog_dict = {
+        'program': 0,
+        'name': 'Program 1',
+        'tab0': 'active',
+        'tab1': '',
+    }
+    if 'pr' in request.args and int(request.args['pr']) == 1:
+        ic()
+        prog_dict['program'] = int(request.args['pr'])
+        prog_dict['name'] = "Program 2"
+        prog_dict['tab0'] = ''
+        prog_dict['tab1'] = 'active'
+    print(prog_dict)
+
+    return render_template('base.html', pr=prog_dict)
+
 
 if __name__ == '__main__':
     app.run()
